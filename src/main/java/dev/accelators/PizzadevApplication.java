9486594794c940000d6de477f4ebe06b28931e82
mbranches <marcusvbranches@gmail.com>
2025-07-10 11:26:59 -0300

    commit inicial

---
+++ src/main/java/dev/accelators/PizzadevApplication.java
@@ -0,0 +1,13 @@
+package dev.accelators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzadevApplication.class, args);
+	}
+
+}
