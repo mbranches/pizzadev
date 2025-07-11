474f9df916d937b1b015f0fbf2c4d1250a4c97a9
mbranches <marcusvbranches@gmail.com>
2025-07-10 22:42:35 -0300

    feat: adiciona annotations de id a PizzaFlavor.java

--- src/main/java/dev/accelators/model/PizzaFlavor.java
+++ src/main/java/dev/accelators/model/PizzaFlavor.java
@@ -1,12 +1,13 @@
 package dev.accelators.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
 @Entity(name = "pizza_flavors")
 public class PizzaFlavor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
