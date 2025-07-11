7cebac9e5c8593eb84ca12cd0ce2cb420d92f73f
mbranches <marcusvbranches@gmail.com>
2025-07-11 11:27:07 -0300

    feat: adiciona model OrderItemExtra

---
+++ src/main/java/dev/accelators/model/OrderItemExtra.java
@@ -0,0 +1,21 @@
+package dev.accelators.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "order_items_extras")
+public class OrderItemExtra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "order_item_id")
+    private OrderItem orderItem;
+    @ManyToOne
+    @JoinColumn(name = "extra_id")
+    private Extra extra;
+    private Integer quantity;
+    @Column(name = "total_value")
+    private BigDecimal totalValue;
+}
