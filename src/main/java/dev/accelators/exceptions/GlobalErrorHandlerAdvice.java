4f25f491d396665228900d3142d71896e118ee19
mbranches <marcusvbranches@gmail.com>
2025-07-11 15:52:17 -0300

    feat: adiciona tratamento para exceções de validações

--- src/main/java/dev/accelators/exceptions/GlobalErrorHandlerAdvice.java
+++ src/main/java/dev/accelators/exceptions/GlobalErrorHandlerAdvice.java
@@ -1,9 +1,14 @@
 package dev.accelators.exceptions;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalErrorHandlerAdvice {
@@ -12,5 +17,20 @@ public class GlobalErrorHandlerAdvice {
         DefaultMessageError response = new DefaultMessageError(HttpStatus.UNAUTHORIZED.value(), e.getReason());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DefaultMessageError> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String defaultMessage = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .filter(Objects::nonNull)
+                .sorted()
+                .collect(Collectors.joining(", "));
+
+        DefaultMessageError errorResponse = new DefaultMessageError(HttpStatus.BAD_REQUEST.value(), defaultMessage);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
